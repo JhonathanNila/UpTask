477c1a812ea39915e5a1544036496b24becd56a1
Jhonathan Nila <nilalimon@gmail.com>
2025-11-04 00:02:25 -0600

    Section 73: Filters functionality finished

--- views/dashboard/project.php
+++ views/dashboard/project.php
@@ -7,6 +7,39 @@
                 id="add-task"
             >&#43; New Task</button>
         </div>
+        <div class="filters" id="filters">
+            <div class="inputs-filters">
+                <h2>Status Filters:</h2>
+                <div class="field">
+                    <label for="all">All Tasks</label>
+                    <input 
+                        type="radio" 
+                        name="filter" 
+                        id="all"
+                        checked
+                        value=""
+                    />
+                </div>
+                <div class="field">
+                    <label for="completed">Completed Tasks</label>
+                    <input 
+                        type="radio" 
+                        name="filter" 
+                        id="completed"
+                        value="1"
+                    />
+                </div>
+                <div class="field">
+                    <label for="new">New Tasks</label>
+                    <input 
+                        type="radio" 
+                        name="filter" 
+                        id="new"
+                        value="0"
+                    />
+                </div>
+            </div>
+        </div>
         <ul id="tasks-list" class="tasks-list"></ul>
     </div>
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
